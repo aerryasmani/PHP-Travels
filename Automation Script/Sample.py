ac02443fbaa3bc3961a2e6c7edde34cc3793ec79
Aerry Asmani <aerryasmani@gmail.com>
2024-01-11 10:21:41 +0800

    Addee button click functionality

--- Automation Script/Sample.py
+++ Automation Script/Sample.py
@@ -21,5 +21,24 @@ try:
 except NoSuchElementException:
     print("Element not found using the provided XPath.")
 
+# Find the button using the defined XPath
+button_xpath = '//*[@id="__next"]/div/header/div/div/div[3]/div[1]/a'
+
+    
+button = driver.find_element("xpath", button_xpath)
+
+# Find the button using the provided XPath
+try:
+    element = button
+    print("Button found using the provided XPath!")
+except NoSuchElementException:
+    print("Button not found using the provided XPath.")
+
+# Click the button
+button.click()
+
+# Add a delay for 10 seconds (you can adjust this as needed)
+time.sleep(20)
+
 # Close the browser window
 driver.quit()
