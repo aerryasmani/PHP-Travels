ee908fbf6b6f37b40c59fb76bb67f5c8de6a0269
Aerry Asmani <aerryasmani@gmail.com>
2024-01-16 23:29:44 +0800

    Open browser

---
+++ Automation Script/LanguangeDropdown.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Open Chrome Browser
+driver = webdriver.Chrome()
+
+# Maximize Browser
+driver.maximize_window()
+
+# Open Website
+driver.get("https://phptravels.net/")
