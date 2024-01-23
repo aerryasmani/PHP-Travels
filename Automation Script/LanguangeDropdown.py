30c8f3f8b9e56912305eff035b1b84121621cbd7
Aerry Asmani <aerryasmani@gmail.com>
2024-01-23 22:21:04 +0800

    Newwww

--- Automation Script/LanguangeDropdown.py
+++ Automation Script/LanguangeDropdown.py
@@ -27,29 +27,6 @@ dropdown_xpath = '//*[@id="navbarSupportedContent"]/div[2]/ul/li[1]/a'
 wait = WebDriverWait(driver, 30)
 
 # Click on the dropdown
-try:
-    dropdown = wait.until(EC.element_to_be_clickable((By.XPATH, dropdown_xpath)))
-    dropdown.click()
-except TimeoutException as e:
-    print(f"TimeoutException: {e}")
-
-#Verify dropdown options
-def verifyTextPresent(text_to_verify, dropdown_options):
-    return text_to_verify in dropdown_options
-
-def verify_dropdown(text_list, dropdown_options):
-    for text_to_verify in text_list:
-        while not verifyTextPresent(text_to_verify, dropdown_options):
-            print(f"Text '{text_to_verify}' not found in the dropdown. Retrying...")
-    print("Verification complete for all texts.")
-
-# Example usage:
-dropdown_options = ["English", "Arabic", "Turkish"]
-
-text_list_to_verify = ["English", "Arabic", "Turkish"]
-
-verify_dropdown(text_list_to_verify, dropdown_options)
-
 #Verify the choosen option
 #Click the option
 #Close dropdown
