ce12ea9750d6d5e9899da1b91e720c3a9eac192f
Aerry Asmani <aerryasmani@gmail.com>
2024-01-23 23:17:28 +0800

    Verify text of dropdown

--- Automation Script/LanguangeDropdown.py
+++ Automation Script/LanguangeDropdown.py
@@ -20,18 +20,30 @@ WebDriverWait(driver, 30).until(
     EC.title_contains("PHPTRAVELS")
 )
 
+time.sleep(5)
+
 # Identify the dropdown element by its XPath
 dropdown_xpath = '//*[@id="navbarSupportedContent"]/div[2]/ul/li[1]/a'
 
-# Increase the timeout for the WebDriverWait
-wait = WebDriverWait(driver, 30)
+# Click on the dropdown
+dropdown = driver.find_element(By.XPATH, dropdown_xpath)
+time.sleep(5)
+dropdown.click()
+
+# Verify the text presents
+element = driver.find_element(By.XPATH, "//body/header/div/div[2]/div[2]/ul/li[1]/ul/li[1]/a")
+search_text = "English"
+if search_text in element.text:
+    print(f"The element contains the {search_text}.")
+else:
+    print(f"The element does not contain the search text: {search_text}.")
 
 # Click on the dropdown
 #Verify the choosen option
 #Click the option
+#Other operations
 #Close dropdown
-
-# Delay to make sure everything is close
+#Delay to make sure everything is close
 time.sleep(5)
 
 # Close the browser window
