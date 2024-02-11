5a0e5f3b1dc98e13840cf63c2703a10f3e73f8e9
Aerry Asmani <aerryasmani@gmail.com>
2024-02-11 19:53:41 +0800

    Changes to xpath

--- Automation Script/Navigation Verification/VerifyTheNavigationRedirection.py
+++ Automation Script/Navigation Verification/VerifyTheNavigationRedirection.py
@@ -34,24 +34,9 @@ for Navs, result in Navigation_results.items():
     else:
         print(f"The element does not contain the navigation option: {Navs}.")
 
-#Click on The Navigation And Redirection
-for Navs in Navigation_options:
-    Navigation_element = driver.find_element(By.XPATH, "//ul[@class='header_menu navbar-nav']/li[position() >= 1 and position() <= 5]/a")
-    Navigation_element.click()
+#Something Something
 
-#Wait for the page to load completely
-WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
 
-try:
-    WebDriverWait(driver, 10).until(EC.url_contains(Navs.lower()))
-    Navigation_results[Navs] = True
-    print(f"The navigation option '{Navs}' is present.")
-except:
-    Navigation_results[Navs] = False
-    print(f"The navigation option '{Navs}' is not present.")
 
-#Delay Time
-time.sleep(10)    
+ 
 
-# Go back to the previous page
-driver.back()
