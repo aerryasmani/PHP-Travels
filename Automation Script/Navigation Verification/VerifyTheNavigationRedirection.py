648cfefeaeb6ec53aafee61561848378c28f78be
Aerry Asmani <aerryasmani@gmail.com>
2024-02-11 18:52:34 +0800

    WIP

--- Automation Script/Navigation Verification/VerifyTheNavigationRedirection.py
+++ Automation Script/Navigation Verification/VerifyTheNavigationRedirection.py
@@ -19,3 +19,39 @@ driver.maximize_window()
 WebDriverWait(driver, 30).until(EC.title_contains("PHPTRAVELS"))
 
 time.sleep(5)
+
+# Verify the navigation text presents
+element = driver.find_element(By.XPATH, "//ul[@class='header_menu navbar-nav']/li[position() >= 1 and position() <= 5]/a")
+Navigation_options = ["Flights", "Hotels", "Tours", "Cars", "Blogs"]
+Navigation_results = {}
+
+for Navs in Navigation_options:
+    Navigation_results[Navs] = Navs in element.text
+
+for Navs, result in Navigation_results.items():
+    if result:
+        print(f"The element contains the navigation option: {Navs}.")
+    else:
+        print(f"The element does not contain the navigation option: {Navs}.")
+
+#Click on The Navigation And Redirection
+for Navs in Navigation_options:
+    Navigation_element = driver.find_element(By.XPATH, "//ul[@class='header_menu navbar-nav']/li[position() >= 1 and position() <= 5]/a")
+    Navigation_element.click()
+
+#Wait for the page to load completely
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
+
+try:
+    WebDriverWait(driver, 10).until(EC.url_contains(Navs.lower()))
+    Navigation_results[Navs] = True
+    print(f"The navigation option '{Navs}' is present.")
+except:
+    Navigation_results[Navs] = False
+    print(f"The navigation option '{Navs}' is not present.")
+
+#Delay Time
+time.sleep(10)    
+
+# Go back to the previous page
+driver.back()
