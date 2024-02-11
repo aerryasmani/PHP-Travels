1c0bb245e808a9c4da9cad8151a1bfe8fb5e8b46
Aerry Asmani <aerryasmani@gmail.com>
2024-02-11 18:16:49 +0800

    Added new test case

---
+++ Automation Script/Navigation Verification/VerifyTheNavigationRedirection.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+# Open Chrome Browser
+driver = webdriver.Chrome()
+driver.delete_all_cookies()
+time.sleep(5)
+
+# Open Website
+driver.get("https://phptravels.net/")
+
+# Maximize Browser
+driver.maximize_window()
+
+# Wait for the page to load
+WebDriverWait(driver, 30).until(EC.title_contains("PHPTRAVELS"))
+
+time.sleep(5)
